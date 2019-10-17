131bdb09732dcd124fd1064b32027368196b165e
alexapple <alexapplemanzer>
2019-10-17 13:13:09 -0400

    new file

---
+++ public_html/Assign-4/Assign-4-b/mysql.php
@@ -0,0 +1,10 @@
+<?php
+
+$res_id=MySQLi_Connect('localhost','amanzer','','amanzer');
+					if(MySQLi_Connect_Errno()) {
+						echo "<tr align='center'> <td colspan='5'> Failed to connect to MySQL </td> </tr>";
+					}
+					else {
+					}
+
+?>
