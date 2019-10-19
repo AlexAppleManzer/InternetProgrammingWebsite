af8bdab72fa82f0ccd35f1c6a7246a453fb682b2
Appl3man@live.com <Appl3man@live.com>
2019-10-19 14:16:05 -0400

    small thing

--- public_html/Assign-4/Assign-4-b/mysql.php
+++ public_html/Assign-4/Assign-4-b/mysql.php
@@ -1,5 +1,5 @@
 <?php
 $config = parse_ini_file('/home/amanzer/private/config.ini');
 $resid=MySQLi_Connect($config['servername'],$config['username'],$config['password'],$config['dbname']);
-
+//skhpa@yahoo.com
 ?>
